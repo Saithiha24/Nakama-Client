89cdef40e6c9daa7a877566fecf7fe526c7b25fa
Sai <sai081118@gmail.comgit commit>
2022-01-02 17:25:10 +0630

    comfirmed pw error

--- src/component/auth/SignIn.js
+++ src/component/auth/SignIn.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import {
   Container,
   Form,
@@ -17,9 +18,38 @@ const SignIn = () => {
   const [loading, setloading] = useState(false);
   const [toast, settoast] = useState(false);
   const [showA, setShowA] = useState(true);
-
+  const [showB, setshowB] = useState(true);
+  const [pwToast, setpwToast] = useState(false);
   // Function
   const toggleShowA = () => setShowA(!showA);
+  const toggleShowB = () => setshowB(!showB);
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setloading(true);
+    if (password !== confirmedPassword) {
+      setloading(false);
+      setpwToast(true);
+    }
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const url = "http://localhost:5000/api/auth/signin";
+      const { data } = axios.post(
+        url,
+        {
+          name,
+          email,
+          password,
+          pic,
+        },
+        config
+      );
+      console.log(data);
+    } catch {}
+  };
   const setProfile = (pics) => {
     if (pics === "undefined") settoast(true);
     if (pics.type === "image/jpeg" || pics.type === "image/jpg") {
@@ -37,9 +67,7 @@ const SignIn = () => {
         .then((data) => {
           setloading(false);
           const picture = data.url.toString();
-          console.log(picture);
           setpic(picture);
-          console.log(pic);
         })
         .catch((error) => {
           console.log(error);
@@ -60,19 +88,37 @@ const SignIn = () => {
             show={showA}
             onClose={toggleShowA}
             className="bg-danger text-white"
+            autohide="false"
+            delay="3000"
           >
             <Toast.Header>
               <strong className="me-auto">Warning</strong>
               <small>11 mins ago</small>
             </Toast.Header>
-            <Toast.Body>
-              Woohoo, you're reading this text in a Toast!
-            </Toast.Body>
+            <Toast.Body>Please select a picture for profile</Toast.Body>
+          </Toast>
+        </ToastContainer>
+      )}
+      {/* Password Toast */}
+      {pwToast && (
+        <ToastContainer className="p-3" position="bottom-center">
+          <Toast
+            show={showB}
+            onClose={toggleShowB}
+            className="bg-danger text-white"
+            autohide="false"
+            delay="3000"
+          >
+            <Toast.Header>
+              <strong className="me-auto">Warning</strong>
+              <small>11 mins ago</small>
+            </Toast.Header>
+            <Toast.Body>Passwords don't match</Toast.Body>
           </Toast>
         </ToastContainer>
       )}
       {/* Form */}
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Group className="mb-3">
           <Form.Label>Name</Form.Label>
           <Form.Control
